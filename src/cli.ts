e115ac5f7cb4ae887a9193695cc3bf1006a99ed4
xiaohuoni <xiaohuoni@dingtalk.com>
2023-01-12 10:16:07 +0800

    feat: init Question

---
+++ src/cli.ts
@@ -0,0 +1,37 @@
+import { Service as CoreService } from "./service";
+import { printHelp, yParser } from "@umijs/utils";
+
+class Service extends CoreService {
+  constructor(opts?: any) {
+    const cwd = process.cwd();
+    super({
+      ...opts,
+      env: process.env.NODE_ENV,
+      cwd,
+      frameworkName: "konos",
+      presets: opts?.presets || [],
+      plugins: opts?.plugins || [],
+    });
+  }
+}
+
+export async function run() {
+  const args = yParser(process.argv.slice(2), {
+    alias: {
+      version: ["v"],
+      help: ["h"],
+    },
+    boolean: ["version"],
+  });
+  try {
+    await new Service({ plugins: [require.resolve("./version")] }).run({
+      name: args._[0],
+      args,
+    });
+  } catch (e: any) {
+    console.log(e);
+    printHelp.exit();
+    process.exit(1);
+  }
+}
+run();
---
+++ src/service.ts
@@ -0,0 +1,5 @@
+// 请手写这个类
+export class Service {
+  constructor(opts?: any) {}
+  async run(opts: { name: string; args?: any }) {}
+}
